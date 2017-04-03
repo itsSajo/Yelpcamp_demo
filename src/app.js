2dd31029e49a43be6d8645ada943e6def175b4ed
Sajo <piotrs.web@hotmail.com>
2017-04-03 22:45:05 +0200

    build structure first commit

---
+++ src/app.js
@@ -0,0 +1,114 @@
+var
+    express      = require("express"),
+    app          = express(),
+    bodyParser   = require("body-parser"),
+    mongoose     = require("mongoose"),
+    Campground   = require("./models/campground"),
+    Comment      = require("./models/comment"),
+    seedDB       = require("./seeds")
+;
+
+// cleaning DB
+seedDB();
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+// SETUP EXPRESS
+app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs")
+// dirname zwraca nam sciezke, gdzie znajduje sie skrypt
+app.use(express.static(__dirname + "/public"))
+
+
+app.get("/", function(req, res){
+  res.render("landing");
+});
+
+// INDEX - show all resources
+app.get("/campgrounds", function(req, res){
+  Campground.find({}, function(err, allCampgrounds){
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds/index", {
+        campgrounds : allCampgrounds
+      })
+    }
+  })
+})
+
+// CREATE - create new resource
+// logika gdzie powstaje nowy campground poprze form w campgrounds/new
+app.post("/campgrounds", function(req, res){
+  var name = req.body.name;
+  var image = req.body.image;
+  var description = req.body.description
+
+  var newCampground = {
+    name        : name,
+    image       : image,
+    description : description
+
+  };
+  // obiekt przekazujemy do bazy danych
+  Campground.create(newCampground, function(err, newCamp){
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds")
+    }
+  })
+})
+
+// NEW - show form to create resource
+// strona ktora wysyla żadanie POST do /campgrounds poprzez form
+app.get("/campgrounds/new", function(req, res){
+  res.render("campgrounds/new")
+})
+
+// SHOW - show ifo about specific resource
+app.get("/campgrounds/:id", function(req, res){
+  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+    if(err){
+      console.log(err);
+    } else {
+      res.render("campgrounds/show", {campground : foundCampground})
+    }
+  });
+})
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+  Campground.findById(req.params.id, function(err, campground){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", {
+        campground : campground
+      });
+    }
+  });
+})
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+  Campground.findById(req.params.id, function(err, campground){
+    if(err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      Comment.create(req.body.comment, function(err, comment){
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      });
+    }
+  })
+});
+
+// Serwer start
+app.listen(3000, function(){
+  console.log("Server is running");
+});
---
+++ src/seeds.js
@@ -0,0 +1,56 @@
+// czyszczenie  i dodawanie sampli do db, niz ciagle reczne jego wstawianie poprzez create() save()
+var moongose     = require("mongoose");
+var Campground   = require("./models/campground");
+var Comment      = require("./models/comment");
+
+
+var data = [
+  {
+    name        : "Test Camp ",
+    image       : "https://farm6.staticflickr.com/5786/20607281024_5c7b3635cc.jpg",
+    description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    name        : "Test Camp 2",
+    image       : "https://farm6.staticflickr.com/5694/21041875770_ffea6404d0.jpg",
+    description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    name        : "Test Camp 3",
+    image       : "https://farm7.staticflickr.com/6082/6142484013_74e3f473b9.jpg",
+    description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod "
+  }
+]
+
+function seedDB() {
+  Campground.remove({}, function(err){
+    if(err){
+      console.log(err);
+    }
+    console.log("removed campgrounds!");
+    data.forEach(function(seed) {
+      Campground.create(seed, function(err, data){
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("added a campground");
+          Comment.create({
+            text   : "Great place!",
+            author : "Homer"
+          }, function(err, comment) {
+            if (err) {
+              console.log(err);
+            } else {
+              data.comments.push(comment)
+              data.save();
+              console.log("Created new comment!");
+            }
+          });
+        }
+      })
+    });
+  });
+
+}
+
+module.exports = seedDB;
