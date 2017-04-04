ce9dcf3c1f4a79001d8ac91296313c0b618dcbe2
Sajo <piotrs.web@hotmail.com>
2017-04-04 20:39:24 +0200

    added Auth system

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 var
-    express      = require("express"),
-    app          = express(),
-    bodyParser   = require("body-parser"),
-    mongoose     = require("mongoose"),
-    Campground   = require("./models/campground"),
-    Comment      = require("./models/comment"),
-    seedDB       = require("./seeds")
+    express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose"),
+    passport      = require('passport'),
+    LocalStrategy = require('passport-local'),
+    Campground    = require("./models/campground"),
+    Comment       = require("./models/comment"),
+    User          = require("./models/user"),
+    seedDB        = require("./seeds")
 ;
-
-// cleaning DB
-seedDB();
-
-mongoose.connect("mongodb://localhost/yelp_camp");
 
 // SETUP EXPRESS
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
-// dirname zwraca nam sciezke, gdzie znajduje sie skrypt
+// dirname zwraca nam sciezke absolutną, gdzie znajduje sie skrypt
 app.use(express.static(__dirname + "/public"))
+
+// cleaning DB
+seedDB();
+
+// PASSPORT SETUP
+app.use(require("express-session")({
+  secret            : "Secret",
+  resave            : false,
+  saveUninitialized : false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser())
+
+//END PASSPORT SETUP
+
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+// MIDDLEWARE CLASS
+app.use(function(req, res, next){
+  // template to be in every called route
+  res.locals.currentUser = req.user;
+  // moving to next middleware (example routeHandler)
+  next();
+})
 
 
 app.get("/", function(req, res){
@@ -31,7 +57,8 @@ app.get("/campgrounds", function(req, res){
       console.log(err);
     } else {
       res.render("campgrounds/index", {
-        campgrounds : allCampgrounds
+        campgrounds : allCampgrounds,
+        currentUser : req.user
       })
     }
   })
@@ -77,7 +104,7 @@ app.get("/campgrounds/:id", function(req, res){
   });
 })
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
   Campground.findById(req.params.id, function(err, campground){
     if (err) {
       console.log(err);
@@ -89,7 +116,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
   });
 })
 
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
   Campground.findById(req.params.id, function(err, campground){
     if(err) {
       console.log(err);
@@ -107,6 +134,47 @@ app.post("/campgrounds/:id/comments", function(req, res) {
     }
   })
 });
+
+app.get("/register", function(req, res){
+  res.render("register")
+})
+
+app.post('/register', function(req, res){
+  var newUser = new User({username : req.body.username});
+  // creating new user in db with hash key as password
+  // register is from a plugin passport-local-moongose from user model
+ // so u can use methods to db
+  User.register(newUser, req.body.password, function(err, user){
+    if(err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/campgrounds")
+    })
+  })
+})
+
+app.get("/login", function(req, res){
+  res.render("login");
+})
+
+app.post("/login", passport.authenticate("local", {
+  successRedirect : "/campgrounds",
+  failureRedirect : "/login"
+}), function(req, res){});
+
+app.get("logout", function(req, res){
+  req.logout();
+  res.redirect("/campgrounds");
+})
+
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
 
 // Serwer start
 app.listen(3000, function(){
