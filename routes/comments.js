c056ce618fe343ba9528dd792f72e5aee0dd75a7
Sajo <piotrs.web@hotmail.com>
2017-04-05 15:36:48 +0200

    Fixed username tag in comments + Added edit routes for comments

--- routes/comments.js
+++ routes/comments.js
@@ -31,15 +31,41 @@ router.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
           // add username and id to comment
           comment.author.id = req.user._id;
           comment.author.username = req.user.username;
+          comment.save();
           campground.comments.push(comment);
           // save comment
           campground.save();
+          console.log(comment);
           res.redirect("/campgrounds/" + campground._id);
         }
       });
     }
   })
 });
+
+router.get("/campgrounds/:id/comments/:comment_id/edit", function(req, res){
+  Comment.findById(req.params.comment_id, function(err, foundComment){
+    if(err) {
+      res.redirect("back");
+    } else {
+      // we already have id of campgorund in req.params.id
+      res.render("comments/edit", {
+        campground_id    : req.params.id,
+        comment          : foundComment
+      })
+    }
+  })
+})
+
+router.put("/campgrounds/:id/comments/:comment_id", function(req, res){
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+    if (err) {
+      res.redirect("back")
+    } else {
+      res.redirect("/campgrounds/" + req.params.id)
+    }
+  })
+})
 
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()){
